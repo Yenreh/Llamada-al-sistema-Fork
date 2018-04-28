dc2cd5d68f6e534e6ae78e8aecaf9e4d05f586a7
Yenreh <36575476+Yenreh@users.noreply.github.com>
2018-04-27 19:10:31 -0500

    Add files via upload

---
+++ ejem1.c
@@ -0,0 +1,27 @@
+ #include <stdio.h>                                                                 
+ #include <stdlib.h>                                                                
+ #include <unistd.h>                                                                
+ #include <string.h>                                                                
+ #include <sys/wait.h>                                                              
+ int                                                                                
+ main(int argc, char*                                                               
+ argv[])                                                                            
+ {                                   
+	while(1){
+		char comando[80];
+		printf("$: ");
+		scanf("%s",comando);
+		printf("digito %s\n",comando);
+		if (!strcmp("exit", comando)){
+			break;
+		}
+		pid_t pid = fork();
+		if(!pid) {
+			char * myargs[2];
+			myargs[0] = strdup(comando); 	//rpogram: "wc" (word count)
+			myargs[1] = NULL;		//marks end of array
+			execvp(myargs[0],myargs);	//runs word count
+		}else{
+			wait(NULL);
+		}
+	}             
